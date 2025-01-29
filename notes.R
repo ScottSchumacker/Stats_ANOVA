07420941ff8575be697399732f762aa165090948
Scott Schumacker <scottschu97@gmail.com>
2025-01-28 20:07:48 -0600

    Updated notes file

--- notes.R
+++ notes.R
@@ -1,11 +1,13 @@
 # Scott Schumacker
 # Notes
 
+# Loading libraries
 library(ggplot2)
 library(dplyr)
 library(gridExtra)
 library(tidyr)
 
+# Creating data set
 DrugA <- rnorm(100, mean = 80, sd = 1)
 DrugB <- rnorm(100, mean = 95, sd = 1)
 DrugC <- rnorm(100, mean = 95, sd = 1)
@@ -19,6 +21,7 @@ df2 <- df %>% pivot_longer(
 )
 head(df2)
 
+# Visualizing data and testing for normality
 ggplot(df2, aes(x = Blood_Pressure, fill = Drug)) +
   geom_histogram(color = "black", alpha = 0.6) +
   theme_classic() +
@@ -28,8 +31,10 @@ shapiro.test(DrugA)
 shapiro.test(DrugB)
 shapiro.test(DrugC)
 
+# Test for equal variance between groups
 bartlett.test(Blood_Pressure ~ Drug, data = df2)
 
+# Running the ANOVA
 analysisOut <- aov(Blood_Pressure ~ Drug, data = df2)
 summary(analysisOut)
 
