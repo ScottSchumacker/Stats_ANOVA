d3cceb069a8ef827feae132a19536c33a7bd124f
Scott Schumacker <scottschu97@gmail.com>
2025-01-28 20:05:51 -0600

    Created notes file

---
+++ notes.R
@@ -0,0 +1,36 @@
+# Scott Schumacker
+# Notes
+
+library(ggplot2)
+library(dplyr)
+library(gridExtra)
+library(tidyr)
+
+DrugA <- rnorm(100, mean = 80, sd = 1)
+DrugB <- rnorm(100, mean = 95, sd = 1)
+DrugC <- rnorm(100, mean = 95, sd = 1)
+
+df <- data.frame(DrugA, DrugB, DrugC)
+
+df2 <- df %>% pivot_longer(
+  cols = `DrugA`:`DrugC`,
+  names_to = "Drug",
+  values_to = "Blood_Pressure"
+)
+head(df2)
+
+ggplot(df2, aes(x = Blood_Pressure, fill = Drug)) +
+  geom_histogram(color = "black", alpha = 0.6) +
+  theme_classic() +
+  xlab("Diastolic Blood Pressure")
+
+shapiro.test(DrugA)
+shapiro.test(DrugB)
+shapiro.test(DrugC)
+
+bartlett.test(Blood_Pressure ~ Drug, data = df2)
+
+analysisOut <- aov(Blood_Pressure ~ Drug, data = df2)
+summary(analysisOut)
+
+TukeyHSD(analysisOut, conf.level = 0.95)
